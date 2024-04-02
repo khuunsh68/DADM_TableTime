f974a066868f22dfde8b8b8afeef2cb620aa149e
khuunsh68 <goncalofarias10@ua.pt>
2024-04-02 11:27:25 +0100

    Activities atidionadas

---
+++ TableTime/app/src/main/java/com/ua/tabletime/ContaInformacoesActivity.kt
@@ -0,0 +1,38 @@
+package com.ua.tabletime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ContaInformacoesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conta_informacoes)
+
+
+        setContentView(R.layout.activity_conta_informacoes)
+
+        // Obtendo referência ao LinearLayout dentro do ScrollView
+        val containerReservas = findViewById<LinearLayout>(R.id.containerReservas)
+
+        // Adicionando manualmente os cartões de reserva
+        for (i in 1..5) {//5 cartões exemplo
+            val cardView = layoutInflater.inflate(R.layout.card_reserva, null)
+
+            val textViewNomeReserva = cardView.findViewById<TextView>(R.id.textViewNomeRestaurante)
+            val textViewQuantidade = cardView.findViewById<TextView>(R.id.textViewQuantidade)
+            val textViewDataReserva = cardView.findViewById<TextView>(R.id.textViewDataReserva)
+            val imageView = cardView.findViewById<ImageView>(R.id.imageView)
+            val imageView2 = cardView.findViewById<ImageView>(R.id.imageView2)
+
+            textViewNomeReserva.text = "Nome do Restaurante $i"
+            textViewQuantidade.text = "$i"
+            textViewDataReserva.text = "0$i/03/2024 - 19h30"
+
+            containerReservas.addView(cardView)
+        }
+    }
+}
---
+++ TableTime/app/src/main/java/com/ua/tabletime/LoginActivity.kt
@@ -0,0 +1,73 @@
+package com.ua.tabletime
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var buttonLogin: Button
+    lateinit var buttonCriarConta: Button
+    lateinit var inputEmail: EditText
+    lateinit var inputPassword: EditText
+    lateinit var msgErro: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        buttonLogin = findViewById(R.id.buttonCriarConta)
+        buttonCriarConta = findViewById(R.id.buttonLogin)
+        inputEmail = findViewById(R.id.editTextEmail)
+        inputPassword = findViewById(R.id.editTextPassword)
+        msgErro = findViewById(R.id.textViewErro)
+
+        buttonLogin.setOnClickListener {
+            val email = inputEmail.text.toString()
+            val password = inputPassword.text.toString()
+            println(email)
+            println(password)
+
+            if (email.isBlank()) {
+                msgErro.text = "Campo de email vazio!"
+                Toast.makeText(applicationContext, "Campo de email vazio!", Toast.LENGTH_LONG).show()
+            } else if (password.isBlank()) {
+                msgErro.text = "Campo de password vazio!"
+                Toast.makeText(applicationContext, "Campo de password vazio!", Toast.LENGTH_LONG).show()
+            } else if (!isValidEmail(email)) {
+                msgErro.text = "Formato de email inválido!"
+                Toast.makeText(applicationContext, "Formato de email inválido!", Toast.LENGTH_LONG).show()
+            } else if (!isValidPassword(password)) {
+                msgErro.text = "A senha deve ter pelo menos 8 caracteres!"
+                Toast.makeText(applicationContext, "A senha deve ter pelo menos 8 caracteres!", Toast.LENGTH_LONG).show()
+            } else {
+                //LOGIN OK, ir para a página inicial
+                startActivity(Intent(this, ContaInformacoesActivity::class.java))
+            }
+        }
+
+        buttonCriarConta.setOnClickListener {
+            /*
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, CreateAccountFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+             */
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 8
+    }
+}
